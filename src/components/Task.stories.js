25edcec245762d3f0079e347e94ed598a3d7e5dc
Casio <hereiscasio@gmail.com>
2020-04-07 13:54:49 +0800

    feat: 🎸 play done with presentation component

---
+++ src/components/Task.stories.js
@@ -0,0 +1,65 @@
+import { action } from '@storybook/addon-actions';
+import Task from './Task';
+export default {
+  title: 'Task',
+  // Our exports that end in "Data" are not stories.
+  // just fucking don't render them in SB
+  excludeStories: /.*Data$/,
+};
+export const actionsData = {
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask'),
+};
+
+export const taskData = {
+  id: '1',
+  title: 'Test Task',
+  state: 'Task_INBOX',
+  updated_at: new Date(2019, 0, 1, 9, 0),
+};
+
+const taskTemplate = `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`;
+
+// default task state
+// Default Component use 'Task' Component
+// like target component wrapper
+// is also like test cases
+// or we can call it Story Component <story/>
+export const Default = () => ({
+  components: { Task },
+  template: taskTemplate,
+  props: {
+    task: {
+      default: () => taskData,
+    },
+  },
+  methods: actionsData,
+});
+// pinned task state
+export const Pinned = () => ({
+  components: { Task },
+  template: taskTemplate,
+  props: {
+    task: {
+      default: () => ({
+        ...taskData,
+        state: 'TASK_PINNED',
+      }),
+    },
+  },
+  methods: actionsData,
+});
+// archived task state
+export const Archived = () => ({
+  components: { Task },
+  template: taskTemplate,
+  props: {
+    task: {
+      default: () => ({
+        ...taskData,
+        state: 'TASK_ARCHIVED',
+      }),
+    },
+  },
+  methods: actionsData,
+});
